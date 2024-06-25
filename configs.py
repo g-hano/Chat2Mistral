ea6caa3c18500890ca66796e19369584a2a8ac05
Cihan Yalçın <113353248+g-hano@users.noreply.github.com>
2024-06-25 11:57:40 +0300

    Update configs.py
    Changed DEVICE to 'auto'

--- configs.py
+++ configs.py
@@ -1,5 +1,3 @@
-import torch
-
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
 EMBEDDING_NAME = "nomic-ai/nomic-embed-text-v1.5"
 TEMPERATURE = 0.0
@@ -11,4 +9,4 @@ SYSTEM_PROMPT = """[INST] You are a helpful assistant that answers
                 user questions using the documents provided. 
                 Your answer MUST be in markdown format without 
                 any prefixes like 'assistant:' [/INST]"""
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = "auto"
