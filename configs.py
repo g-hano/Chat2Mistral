f917932ed30ccd7f9171807fe7a064638ab1442f
Cihan Yalçın <113353248+g-hano@users.noreply.github.com>
2024-07-01 16:38:58 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,3 +1,5 @@
+import torch
+
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
 EMBEDDING_NAME = "nomic-ai/nomic-embed-text-v1.5"
 TEMPERATURE = 0.0
@@ -9,4 +11,4 @@ SYSTEM_PROMPT = """[INST] You are a helpful assistant that answers
                 user questions using the documents provided. 
                 Your answer MUST be in markdown format without 
                 any prefixes like 'assistant:' [/INST]"""
-DEVICE = "auto"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
