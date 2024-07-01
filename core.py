281c7381d7bd1927f68213e54636103ff6c650bb
Cihan Yalçın <113353248+g-hano@users.noreply.github.com>
2024-07-01 20:39:45 +0300

    Update core.py

--- core.py
+++ core.py
@@ -23,7 +23,7 @@ from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline, HuggingF
 
 pipe = HuggingFacePipeline.from_model_id(
                            model_id=MODEL_NAME,
-                           device=0,
+                           device_map="auto",
                            task="text-generation")
 llm = ChatHuggingFace(llm=pipe)
 
