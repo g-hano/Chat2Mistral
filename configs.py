ce1b7c687aaa146034e37028490fe610e28cb657
Cihan Yalçın <113353248+g-hano@users.noreply.github.com>
2024-07-05 18:44:29 +0300

    more handsome
    added logging info, removed temperature

--- configs.py
+++ configs.py
@@ -1,9 +1,9 @@
 import torch
+import logging
+logging.basicConfig(level=logging.INFO)
 
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
 EMBEDDING_NAME = "nomic-ai/nomic-embed-text-v1.5"
-TEMPERATURE = 0.0
-CONTEXT_WINDOW = 32_000
 TOP_K = 5
 CHUNK_SIZE = 512
 CHUNK_OVERLAP = 10
@@ -11,6 +11,7 @@ SYSTEM_PROMPT = """[INST] You are a helpful assistant that answers
                 user questions using the documents provided. 
                 Your answer MUST be in markdown format without 
                 any prefixes like 'assistant:' [/INST]"""
-running_on = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"---Running on {running_on}---")
 DEVICE = "auto"
+
+running_on = "cuda" if torch.cuda.is_available() else "cpu"
+logging.info(f"----Running on {running_on}----")
